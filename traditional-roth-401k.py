fd82b4cb84ce34bf9cb7908991d9f71d82b0604f
Aaron <aaronhjlee1@gmail.com>
2020-12-28 22:12:29 -0800

    created default filters and cleaned up functions (OOP)

--- traditional-roth-401k.py
+++ traditional-roth-401k.py
@@ -1,19 +1,35 @@
 '''
 Last updated 2020-12-28
+Edits:
+1. added the manual inputs
+2. clean up functions to just use self rather than multiple arguements
+
+Future additions:
+1. add a line for how much people will have at the end
+2. an ending contribution line
+    2a. recommendation of when to lower or stop contribution
+3. add visualizations to automatically show up after function is run
+    3a. plotly?
+    3b. interactive?
 '''
 
 import pandas as pd
 import numpy as np
 
 class profit():
-    def __init__(self):
-        pass
+    def __init__(self, age=30, retirement_age=65, annual_contribution=19000, percent_return=1.075, percent_return_non_401k=1.08, base_salary=100000):
+        self.age = age
+        self.retirement_age = retirement_age
+        self.annual_contribution = annual_contribution
+        self.percent_return = percent_return
+        self.percent_return_non_401k = percent_return_non_401k
+        self.base_salary = base_salary
 
-    def roth_401k(self, years, annual_contribution, percent_return):
-        res = annual_contribution
-        annual_list =[annual_contribution]
+    def roth_401k(self, years):
+        res = self.annual_contribution
+        annual_list =[self.annual_contribution]
         for i in range(years):
-            res = annual_contribution + annual_list[i] * percent_return
+            res = self.annual_contribution + annual_list[i] * self.percent_return
             annual_list.append(int(res))
         return annual_list
 
@@ -25,32 +41,32 @@ class profit():
             res.append(int(i*tax_low))
         return res
 
-    def tax_bracket(self, base_salary, annual_contribution):
+    def tax_bracket(self):
         # if no tax bracket change, then go with roth 100% -- if bracket changes, put as much in traditional to lower tax bracket
     
         bracket_list = [9875, 40125, 85525, 163300, 207350, 518400]
         tax_list = [0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37]
-        diff = base_salary - annual_contribution
+        diff = self.base_salary - self.annual_contribution
 
         if diff > bracket_list[5]:
             traditional_contribution = 0
-            roth_contribution = annual_contribution
+            roth_contribution = self.annual_contribution
         else:
             for i in range(4, 0, -1):
                 if bracket_list[i] < diff <= bracket_list[i+1]:
-                    if base_salary - bracket_list[i+1] < 0:
+                    if self.base_salary - bracket_list[i+1] < 0:
                         tax_up = tax_list[i+1]
                         tax_low = tax_list[i]
                         traditional_contribution = 0
-                        roth_contribution = annual_contribution
+                        roth_contribution = self.annual_contribution
                         print('Your tax bracket is: {}%'.format(tax_up))
                         print('Contribute ${} to your traditional 401k'.format(traditional_contribution))
                         print('You should contribute {} to your roth 401k at your current tax rate of {}%'.format(roth_contribution, tax_up))
                     else:
                         tax_up = tax_list[i+2]
                         tax_low = tax_list[i+1]
-                        traditional_contribution = base_salary - bracket_list[i+1]
-                        roth_contribution = annual_contribution - (base_salary - bracket_list[i+1])
+                        traditional_contribution = self.base_salary - bracket_list[i+1]
+                        roth_contribution = self.annual_contribution - (self.base_salary - bracket_list[i+1])
                         print('Your upper tax is: {}%'.format(tax_up))
                         print('Your lower tax is: {}%'.format(tax_low))
                         print('You can lower your tax bracket to {}% by contributing to your traditional 401k: ${}'.format(tax_low, traditional_contribution))
@@ -65,14 +81,14 @@ class profit():
         return (1-tax_up), (1-tax_low)
         
 
-    def brokerage_list(self, years, annual_contribution, percent_return_non_401k, base_salary, tax_up, tax_low):
-        roth = base_salary*tax_up - annual_contribution
-        traditional = (base_salary-annual_contribution)*tax_low  
+    def brokerage_list(self, years, tax_up, tax_low):
+        roth = self.base_salary*tax_up - self.annual_contribution
+        traditional = (self.base_salary-self.annual_contribution)*tax_low  
         difference = traditional - roth
         
         annual_trad_list =[difference]
         for i in range(years):
-            res = difference + annual_trad_list[i] * percent_return_non_401k
+            res = difference + annual_trad_list[i] * self.percent_return_non_401k
             annual_trad_list.append(int(res))
         return annual_trad_list
 
@@ -89,26 +105,25 @@ class profit():
     --------------------------------------------------------------------------------------------------
     '''
 
-    # def howdy_401k(self, age, retirement_age, annual_contribution, percent_return, percent_return_non_401k, base_salary):
-    def howdy_401k(self, age=30, retirement_age=65, annual_contribution=19000, percent_return=1.075, percent_return_non_401k=1.08, base_salary=100000):
-        print('Your base salary is ${}'.format(base_salary))
-        years = retirement_age-age
+    def howdy_401k(self):
+        print('Your base salary is ${}'.format(self.base_salary))
+        years = self.retirement_age-self.age
         
         # make the table
         df = pd.DataFrame(list(range(years+1)), columns=['years'])
         
         # find tax range
-        tax_up, tax_low = self.tax_bracket(base_salary, annual_contribution)
+        tax_up, tax_low = self.tax_bracket()
 
         # create the return for 401k
-        df['401k_roth'] = self.roth_401k(years, annual_contribution, percent_return)
-        df['401k_traditional (pre_tax)'] = self.roth_401k(years, annual_contribution, percent_return)
+        df['401k_roth'] = self.roth_401k(years)
+        df['401k_traditional (pre_tax)'] = self.roth_401k(years)
         
         # create the return post tax traditional
         df['401k_traditional (post_tax)'] = self.traditional_401k_post_tax(df, tax_low)
         
         # create brokerage by getting the difference and capturing oppurtunity cost
-        brokerage = self.brokerage_list(years, annual_contribution, percent_return, base_salary, tax_up, tax_low)
+        brokerage = self.brokerage_list(years, tax_up, tax_low)
         df['lost roth 401k oppurtunity_cost'] = brokerage    
         
         # create brokerage returns post tax
@@ -129,14 +144,14 @@ def user_input():
     return age, retirement_age, annual_contribution, percent_return, percent_return_non_401k, base_salary
 
 def run_m():
-    mod = profit()
     age, retirement_age, annual_contribution, percent_return, percent_return_non_401k, base_salary = user_input()
-    df = mod.howdy_401k(age, retirement_age, annual_contribution, percent_return, percent_return_non_401k, base_salary)
+    mod = profit(age, retirement_age, annual_contribution, percent_return, percent_return_non_401k, base_salary)
+    df = mod.howdy_401k()
 
     return df
 
 
-# use this to run the model in your notebook or terminal
-# mod = profit()
-# df = mod.howdy_401k(age=30, retirement_age=65, annual_contribution=19000, percent_return=1.075, percent_return_non_401k=1.08, base_salary=100000)
+# use this to run the model in your notebook or terminal with the following values as default
+# mod = profit(age=30, retirement_age=65, annual_contribution=19000, percent_return=1.075, percent_return_non_401k=1.08, base_salary=100000)
+# df = mod.howdy_401k()
 # df
